14de59925743fec41f8d5e6e813813a3c67df932
artt <art.tosborvorn@gmail.com>
2024-12-08 02:07:25 +0700

    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
-import sitemap from "@astrojs/sitemap";
-
-const locales = { th: "th-TH", en: "en-US" }
-const defaultLocale = "th"
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,21 +10,4 @@ export default defineConfig({
   },
   output: 'server',
   adapter: vercel(),
-  integrations: [
-    i18n({
-      locales,
-      defaultLocale,
-      exclude: [
-        "pages/api/**/*",
-        "pages/**/[[]*[]].astro",
-      ],
-    }),
-    sitemap({
-      i18n: {
-        locales,
-        defaultLocale,
-      },
-      filter: filterSitemapByDefaultLocale({ defaultLocale }),
-    }),
-  ],
 });
